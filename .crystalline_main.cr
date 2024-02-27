09990acc0373784418483785a9951e0352760829
SeleneCosmia <hebe@selenocosmia.space>
2024-02-27 07:19:05 -0800

    Minor updates

---
+++ .crystalline_main.cr
@@ -0,0 +1,4 @@
+require "./src/moissanite"
+require "./src/moissanite/colors"
+require "./src/moissanite/window"
+require "./lib/raylib-cr/src/raylib-cr"
--- src/moissanite/window.cr
+++ src/moissanite/window.cr
@@ -1,34 +1,9 @@
-require "raylib-cr"
-require "raylib-cr/rlgl"
-require "raylib-cr/raygui"
 require "./colors"
+require "raylib-cr/rlgl"
+require "raylib-cr"
 sw = 1920
 sh = 1080
-
 alias Rl = Raylib
-alias Rg = Raygui
-alias DP = Raygui::DefaultProperty
-
-class Swatches
-  MAX = 16
-  class_getter boxes = [] of Rl::Rectangle
-
-  def self.setup
-    #  defines instance var boxes
-    #  where boxes is an empty array that indexes Rl::Rectangle
-    @@boxes = [] of Rl::Rectangle
-
-    MAX.times do
-        x = Rl.get_random_value(0, sw).to_f
-        y = Rl.get_random_value(0, sh).to_f
-        width = Rl.get_random_value(10, 100).to_f
-        height = Rl.get_random_value(10, 100).to_f
-        @@boxes << Rl::Rectangle.new(x: x, y: y, width: width, height: height)
-    end
-
-    @@boxes = boxes
-  end
-end
 
 show_text = false
 
@@ -40,31 +15,18 @@ until Rl.close_window?
   Rl.clear_background(Rl::RAYWHITE)
   Rl.draw_rectangle(sw/2, sh/2, sw/4 - 220, sh/4 - 80, Moissanite::ONYX)
 
-  bw = 250
-  bh = 80
-
-  Rg.set_style(Rg::Control::Default, DP::TextSize, 25)
-
-  if Rg.button(
-    Rl::Rectangle.new(
-      x: Rl.get_screen_width/2 - bw/2,
-      y: Rl.get_screen_height/2 - bh/2,
-      width: bw,
-      height: bh
-    ),
-      "This is a button!"
-    )
+  if Rl.key_pressed?(Rl::KeyboardKey::One)
+    Rl.draw_rectangle(sw/2*4, sh/2, 240.0, 135.0, Rl::ORANGE)
     show_text = !show_text
   end
 
   if show_text
-    Rl.draw_text("Button Pressed!", 10, 10, 40, Rl::SKYBLUE)
+    Rl.draw_text("Pressed <1> key", sw/2*4 - 30, sh/2 + 34, 24, Rl::DARKBROWN)
   end
-  
-#  if Rl.key_pressed?(Rl::KeyboardKey::One)
-#      Rl.draw_rectangle(sw/2*4, sh/2, 240.0, 135.0, Rl::ORANGE)
-#      Rl.draw_text("Pressed <1> key", sw/2*4 - 30, sh/2 + 34, 24, Rl::DARKBROWN)
-#  end
+
+  if Rl.key_pressed_repeat?(Rl::KeyboardKey::One)
+    Rl.clear_background(Rl::RAYWHITE)
+  end
 
   Rl.end_drawing
 end
